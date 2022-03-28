02f4e8b1eaf3f95a4185d3dcd9ace6759d212ab6
MateCon <mateoziffer@gmail.com>
2022-03-28 08:59:14 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express, { Application, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 const port: number = 8080;
 
 app.get('/', (_, res: Response) => {
     res.status(200).send('hello world!');
+});
+
+app.get('/user/:name', (req: Request, res: Response) => {
+    const params = req.params;
+    res.send(params.name);
 });
 
 app.listen(port, () => {
