8ded0e5029dfd93618590fc8a4e12a400710ce1d
MateCon <mateoziffer@gmail.com>
2022-03-28 08:51:35 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+import express, { Application, Response } from 'express';
+const app: Application = express();
+const port: number = 8080;
 
-app.get('/', (_: any, res: any) => {
-    res.status(200).send('hello');
+app.get('/', (_, res: Response) => {
+    res.status(200).send('hello world!');
 });
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    console.log(`listening on http://localhost:${port}`);
 });
