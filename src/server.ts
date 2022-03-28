1b2c4e22755b3e57eb96800a23bbd15517840eb1
MateCon <mateoziffer@gmail.com>
2022-03-28 09:30:15 -0300

    add static files

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 const port: number = 8080;
 
+app.use(express.static('public'));
+
 app.get('/', (_, res: Response) => {
     res.status(200).send('hello world!');
 });
 
 app.get('/user/:name', (req: Request, res: Response) => {
     const params = req.params;
-    res.send(params.name);
+    res.status(200).send(params.name);
 });
 
 app.listen(port, () => {
