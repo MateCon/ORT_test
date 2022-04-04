3245229b9676bbe3af03bde379d4ff01e124f621
MateCon <mateoziffer@gmail.com>
2022-04-04 07:57:47 -0300

    comment log

--- src/ejercicios/ejercicio_1.ts
+++ src/ejercicios/ejercicio_1.ts
@@ -2,4 +2,4 @@
 
 const f = (list: number[]) => list.filter((a) => a % 2 !== 0).sort((a, b) => b - a)[1];
 
-console.log(f([11, 33, 2, -1, 110, 99, 8]));
+// console.log(f([11, 33, 2, -1, 110, 99, 8]));
