aed59c13f6aecb27b601416e69fec39ff886d9cc
MateCon <mateoziffer@gmail.com>
2022-04-04 10:40:56 -0300

    .

--- src/ejercicios/ejercicio_2.ts
+++ src/ejercicios/ejercicio_2.ts
@@ -1,7 +1,29 @@
-// consigna
+/* 
+Dado un array 
+multiplicar todos sus valores por el anterior 
+menos el siguiente 
+*/
 
-const f = () => {
-    return null;
+export const f1 = (data: number[]) => {
+    const new_data = [];
+    for (let i = 0; i < data.length; i++) {
+        const anterior = data[i - 1] || 0;
+        const siguiente = data[i + 1] || 0;
+        new_data.push(anterior * data[i] - siguiente);
+    }
+    return new_data;
 };
 
-export default f;
+export const f2 = (data: number[]) =>
+    data.map((val, i) => {
+        const anterior = data[i - 1] || 0;
+        const siguiente = data[i + 1] || 0;
+        return anterior * val - siguiente;
+    });
+
+const f3 = (data: number[]) =>
+    data.map((val, i) => (data[i - 1] || 0) * val - (data[i + 1] || 0));
+
+// console.log(f3([2, 3, 5, 12, 54, 6, -1, 0, 4, 23, 66, 7]));
+
+export default f3;
