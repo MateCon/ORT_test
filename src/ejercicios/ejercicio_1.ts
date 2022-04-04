9f002959770eeec17f038fea1d1070019aec78ed
MateCon <mateoziffer@gmail.com>
2022-04-04 08:28:23 -0300

    add export to ejercicio_1

--- src/ejercicios/ejercicio_1.ts
+++ src/ejercicios/ejercicio_1.ts
@@ -1,5 +1,8 @@
 // De la lista 11, 33, 2, -1, 110, 99, 8 obtener el segundo mayor impar
 
-const f = (list: number[]) => list.filter((a) => a % 2 !== 0).sort((a, b) => b - a)[1];
+const f = (list: number[]) =>
+    list.filter((a) => a % 2 !== 0).sort((a, b) => b - a)[1];
 
 // console.log(f([11, 33, 2, -1, 110, 99, 8]));
+
+export default f;
