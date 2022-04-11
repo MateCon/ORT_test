9d3751f0d4447c52b88dd6ef45345c660a3a9ee5
MateCon <mateoziffer@gmail.com>
2022-04-11 07:59:11 -0300

    .

--- src/ejercicios/ejercicio_3.ts
+++ src/ejercicios/ejercicio_3.ts
@@ -1,11 +1,11 @@
 // Duplicar el array anterior y aplicarle la function raiz a todos los elementos,
 
-const f = (list: number[]) => 
+const f = (list: number[]) =>
     [...list, ...list]
-        .filter(n => n >= 0)
-        .map(n => ({
+        .filter((n) => n >= 0)
+        .map((n) => ({
             original: n,
-            modificado: Math.floor(Math.sqrt(n) * 100) / 100
+            modificado: Math.floor(Math.sqrt(n) * 100) / 100,
         }));
 
 // console.log(f([2, 3, 5, 12, 54, 6, -1, 0, 4, 23, 66, 7]));
