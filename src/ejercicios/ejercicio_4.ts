12b2c5468c982d30d584dfffc8cef50ba7fb5992
MateCon <68882595+MateCon@users.noreply.github.com>
2022-04-11 19:14:35 -0300

    ejercicio 4

--- src/ejercicios/ejercicio_4.ts
+++ src/ejercicios/ejercicio_4.ts
@@ -2,10 +2,10 @@
 1) Calcula los años bisiestos que habra antes de 2150
 2) Cuantas cartas con numero par y negras hay en una baraja de poker
 3) eliminar 6 cartas de la baraja al azar y calcular en promedio 
-(haciendo 1000 iteraciones)cuanto valen las cartas (con numero) que quedan.
+(haciendo 1000 iteraciones) cuanto valen las cartas (con numero) que quedan.
 */
 
-const ej_1 = (n: number) => {
+export const ej_1 = (n: number) => {
     let count = 0;
     for (let i = 1; i < n; i++) {
         if (i % 4 === 0 && (!(i % 100 === 0) || i % 400 === 0)) count++;
@@ -13,13 +13,13 @@ const ej_1 = (n: number) => {
     return count;
 };
 
-const ej_1_v2 = (n: number) =>
+export const ej_1_v2 = (n: number) =>
     Math.floor(n / 4) - Math.floor(n / 100) + Math.floor(n / 400);
 
-console.log(ej_1(2050));
-console.log(ej_1_v2(2050));
+// console.log(ej_1(2050));
+// console.log(ej_1_v2(2050));
 
-interface Carta {
+export interface Carta {
     num: number;
     color: 'red' | 'black';
 }
@@ -35,7 +35,7 @@ const generarMazo = (): Carta[] => {
     return mazo;
 };
 
-const ej_2 = () => {
+export const ej_2 = () => {
     const mazo: Carta[] = generarMazo();
     let count = 0;
 
@@ -47,13 +47,20 @@ const ej_2 = () => {
     return count;
 };
 
-console.log(ej_2());
+// console.log(ej_2());
 
-const ej_3 = () => {
+export const ej_3 = () => {
     const mazo: Carta[] = generarMazo();
     let sum = 0;
 
-    // terminar
+    for (let i = 0; i < 1000; i++) {
+        const copy: Carta[] = [...mazo];
+        for (let i = 0; i < 6; i++)
+            copy.splice(Math.floor(Math.random() * mazo.length), 1);
+        for (let card of copy) sum += card.num;
+    }
+
+    return sum / 52000;
 };
 
-console.log(ej_3());
+// console.log(ej_3());
